a268d104bb6e93e4d2a3e76eac33e0fcfdc09bf7
sazwqa <sazwqa@gmail.com>
2011-12-05 19:15:23 +0530

    minor changes

--- src/com/vinsol/sms_scheduler/activities/SmsSchedulerExplActivity.java
+++ src/com/vinsol/sms_scheduler/activities/SmsSchedulerExplActivity.java
@@ -29,7 +29,6 @@ import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.ExpandableListView.ExpandableListContextMenuInfo;
 import android.widget.ExpandableListView.OnChildClickListener;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
