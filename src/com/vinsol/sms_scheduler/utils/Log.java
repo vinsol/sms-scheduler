e9b5117baed018ca68170858edda7196f6778215
sazwqa <sazwqa@gmail.com>
2011-12-05 15:23:25 +0530

    minor AndroidManifest.xml re-arrangement, log added

---
+++ src/com/vinsol/sms_scheduler/utils/Log.java
@@ -0,0 +1,22 @@
+package com.vinsol.sms_scheduler.utils;
+
+public class Log  {
+	
+	private static boolean DEBUG = true;
+    private static String app = "SMSScheduler";
+
+    public static final void d(Throwable throwable) {
+    	if (DEBUG)
+    		android.util.Log.d(app, "", throwable);
+    }
+
+    public static final void d(Object object) {
+    	if (DEBUG)
+    		android.util.Log.d(app, object!=null ? object.toString() : null);
+    }
+
+    public static final void d(Object object, Throwable throwable) {
+    	if (DEBUG)
+    		android.util.Log.d(app, object!=null ? object.toString() : null, throwable);
+    }
+}
