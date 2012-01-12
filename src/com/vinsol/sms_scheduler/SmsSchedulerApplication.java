faa97f960a376d49236e7c2f4c71e6fd3f799ad5
Alok Vaish <alok@alok-PC.(none)>
2012-01-12 12:28:28 +0530

    SmsSchedulerApplication: unused code removed

--- src/com/vinsol/sms_scheduler/SmsSchedulerApplication.java
+++ src/com/vinsol/sms_scheduler/SmsSchedulerApplication.java
@@ -1,19 +1,13 @@
 package com.vinsol.sms_scheduler;
 
 import java.util.ArrayList;
-import com.vinsol.sms_scheduler.models.Contact;
-import android.app.Activity;
+
 import android.app.Application;
 
-public class SmsSchedulerApplication extends Application{
+import com.vinsol.sms_scheduler.models.Contact;
+
+public class SmsSchedulerApplication extends Application {
 
 	public static ArrayList<Contact> contactsList = new ArrayList<Contact>();
 	public static boolean isDataLoaded = false;
-	Activity myActivity = new Activity();
-	
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		
-	}		
 }
